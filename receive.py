af5e5806e2fbd851694d6287b8cf8fc1122ec0e3
mclarasb <mclarasb@motorola.com>
2020-11-18 16:33:46 -0300

    upload send.py and receive.py

---
+++ receive.py
@@ -0,0 +1,27 @@
+import pika, sys, os
+
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(' [x] Received %r' % body.decode())
+
+    channel.basic_consume(callback, queue='hello', no_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
